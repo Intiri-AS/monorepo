bfd730bbf131476232e5cf8f5eac8c90b5b48b38
milos.stolica <milos.stolica@creen.io>
2022-09-06 10:06:40 +0200

    Comment out using static files

--- Intiri.API/Intiri.API/Program.cs
+++ Intiri.API/Intiri.API/Program.cs
@@ -72,12 +72,13 @@ try
 
 	app.UseHttpsRedirection();
 
-	app.UseStaticFiles();
-	app.UseStaticFiles(new StaticFileOptions()
-	{
-		FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
-		RequestPath = new PathString("/Resources")
-	});
+	//app.UseStaticFiles();
+	//app.UseStaticFiles(new StaticFileOptions()
+	//{
+	//	FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+	//	RequestPath = new PathString("/Resources")
+	//});
+
 	// seed data
 	logger.Debug("Seeding data...");
 	await SeedData.SeedTestData(unitOfWork, userManager, roleManager);
