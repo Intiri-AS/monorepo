6f937eae39b6629c2cbadfcb567791a45eb2bbf2
bakiStrahinja <strahinja.baki@mvsoft.rs>
2022-09-07 08:46:26 +0200

    #122 commit fixes

--- Intiri.Client/src/app/pages/end-user-pages/my-intiri/my-intiri.page.ts
+++ Intiri.Client/src/app/pages/end-user-pages/my-intiri/my-intiri.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-my-intiri-page',
@@ -73,12 +72,6 @@ export class MyIntiriPage {
     },
   ]
 
-  constructor(
-    private nav: NavController
-  ) {}
-
-  details() {
-    this.nav.navigateRoot('/project-details');
-  }
+  constructor() {}
 
 }
