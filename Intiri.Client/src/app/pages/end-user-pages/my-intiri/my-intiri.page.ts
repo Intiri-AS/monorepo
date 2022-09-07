96f88e0521263313f7d6f71f24e8616721ee3924
Strahinja Baki <strahinjab@creen.io>
2022-09-07 08:58:45 +0000

    Merged PR 21: #122 My intiri page UI
    #122 My intiri page UI
    
    Related work items: #121, #122

--- Intiri.Client/src/app/pages/end-user-pages/my-intiri/my-intiri.page.ts
+++ Intiri.Client/src/app/pages/end-user-pages/my-intiri/my-intiri.page.ts
@@ -8,6 +8,70 @@ import { Component } from '@angular/core';
 
 export class MyIntiriPage {
 
+  projects = [
+    {
+      projectName: 'Vintage',
+      moodboard: '2',
+      pieces: '20',
+      updated: '1 week',
+      image: '../../../../assets/images/landing-img.png'
+    },
+    {
+      projectName: 'Industrial',
+      moodboard: '3',
+      pieces: '25',
+      updated: '1 day',
+      image: '../../../../assets/images/landing-img.png'
+    },
+    {
+      projectName: 'Minimal',
+      moodboard: '1',
+      pieces: '5',
+      updated: '3 week',
+      image: '../../../../assets/images/landing-img.png'
+    },
+    {
+      projectName: 'Funky',
+      moodboard: '0',
+      pieces: '20',
+      updated: '1 day',
+      image: '../../../../assets/images/landing-img.png'
+    },
+  ];
+
+  news = [
+    {
+      image: '../../../../assets/images/landing-img.png',
+      name: 'Andy Miller',
+      news: 'New moodboard'
+    },
+    {
+      image: '../../../../assets/images/landing-img.png',
+      name: 'Andy Miller',
+      news: 'New moodboard'
+    },
+    {
+      image: '../../../../assets/images/landing-img.png',
+      name: 'Andy Miller',
+      news: 'New moodboard'
+    },
+    {
+      image: '../../../../assets/images/landing-img.png',
+      name: 'Andy Miller',
+      news: 'New moodboard'
+    },
+    {
+      image: '../../../../assets/images/landing-img.png',
+      name: 'Andy Miller',
+      news: 'New moodboard'
+    },
+    {
+      image: '../../../../assets/images/landing-img.png',
+      name: 'Andy Miller',
+      news: 'New moodboard'
+    },
+  ]
+
   constructor() {}
 
 }
---
+++ Intiri.Client/src/app/pages/end-user-pages/project-details/project-details.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-details-page',
+  templateUrl: './project-details.page.html',
+  styleUrls: ['./project-details.page.scss'],
+})
+
+export class ProjectDetailsPage {
+
+  constructor() {}
+
+}
