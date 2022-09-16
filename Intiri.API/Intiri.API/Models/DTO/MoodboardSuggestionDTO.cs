110832178db2e11964cdd3afe9b5a578f9975791
Sinisa Vidovic <sinisa.vidovic@creen.io>
2022-09-16 13:17:36 +0200

    Fix for moodboard match output

--- Intiri.API/Intiri.API/Models/DTO/MoodboardSuggestionDTO.cs
+++ Intiri.API/Intiri.API/Models/DTO/MoodboardSuggestionDTO.cs
@@ -1,10 +1,11 @@
-﻿using Intiri.API.Models.DTO.OutputDTO.Room;
+﻿using Intiri.API.Models.DTO.OutputDTO;
+using Intiri.API.Models.DTO.OutputDTO.Room;
 
 namespace Intiri.API.Models.DTO
 {
 	public class MoodboardSuggestionDTO
 	{
 		public IEnumerable<MoodboardMatchDTO> Moodboards { get; set; }
-		public IEnumerable<RoomOutDTO> Rooms { get; set; }
+		public IEnumerable<MoodboardOutDTO> MoodboardFamily { get; set; }
 	}
 }
