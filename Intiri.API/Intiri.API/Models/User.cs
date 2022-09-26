27a8a62a63fe93c3ba819e7217dc6981931ff2cc
Predrag Seizovic <predrag.seizovic@creen.io>
2022-09-26 05:38:44 +0200

    Task#291: User model expanded. Update databse with new migration

--- Intiri.API/Intiri.API/Models/User.cs
+++ Intiri.API/Intiri.API/Models/User.cs
@@ -6,9 +6,17 @@ namespace Intiri.API.Models
 	{
 		#region Properties
 
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
 		public DateTime Created { get; set; } = DateTime.UtcNow;
-		public string Address { get; set; }
+		public string Gender { get; set; }
+		public string StreetAddress { get; set; }
+		public string PostalCode { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
 		public ICollection<UserRole> Roles { get; set; }
+		public ICollection<Project> CreatedProjects { get; set; }
+		public ICollection<Moodboard> CreatedMoodboards { get; set; }
 
 		#endregion Properties
 	}
