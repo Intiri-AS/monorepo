99daf71260f3919b0b7431a1bba1c30038a36151
Predrag Seizovic <predrag.seizovic@creen.io>
2022-10-04 12:04:19 +0200

    Set production mode

--- Intiri.Client/src/environments/environment.ts
+++ Intiri.Client/src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7004/'
+  production: true,
+  //apiUrl: 'https://localhost:7004/'
+  apiUrl: 'https://intiri-test.azurewebsites.net/'
 };
 
 /*
