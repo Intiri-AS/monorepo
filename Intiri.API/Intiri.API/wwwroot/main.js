dd3a8ca1667cb40473422b50821b10bde6871653
Predrag Seizovic <predrag.seizovic@creen.io>
2022-10-04 12:13:11 +0200

    Production mode build

--- Intiri.API/Intiri.API/wwwroot/main.js
+++ Intiri.API/Intiri.API/wwwroot/main.js
@@ -6736,8 +6736,9 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7004/'
+    production: true,
+    //apiUrl: 'https://localhost:7004/'
+    apiUrl: 'https://intiri-test.azurewebsites.net/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
