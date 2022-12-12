7dc51e2008b500ec32ee50bb172af816d25a1f97
bakiStrahinja <strahinja.baki@mvsoft.rs>
2022-12-12 16:27:26 +0100

    All MVP Fixes

--- Intiri.Client/src/app/pages/designer-pages/my-moodboard/my-moodboard.page.ts
+++ Intiri.Client/src/app/pages/designer-pages/my-moodboard/my-moodboard.page.ts
@@ -1,7 +1,8 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonSlides, PopoverController } from '@ionic/angular';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { MenuPopoverComponent } from 'src/app/components/menu-popover/menu-popover.component';
 import { Moodboard } from 'src/app/models/moodboard.model';
@@ -15,7 +16,7 @@ import { StyleService } from 'src/app/services/style.service';
   styleUrls: ['./my-moodboard.page.scss'],
 })
 
-export class MyMoodboardPage implements OnInit {
+export class MyMoodboardPage implements OnInit, OnDestroy {
   @ViewChild('slides') slides: IonSlides;
 
   options = {
@@ -38,6 +39,7 @@ export class MyMoodboardPage implements OnInit {
     this.innerWidth = event.target.innerWidth;
   }
 
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private _route: ActivatedRoute,
@@ -54,19 +56,27 @@ export class MyMoodboardPage implements OnInit {
     this.moodboardService.getMoodboards();
     this.styleService.getStyles();
 
-    this.moodboards$.subscribe(moodboards => {
-      this.spinner.hide();
-      this.moodboards = moodboards;
-      this.accountService.currentUser$.subscribe((loggedUser) => {
-        this.myMoodboards = this.moodboards.filter(m => m.designer.id === loggedUser.id)
-      });
-    });
+    this.subscriptions.push(
+      this.moodboards$.subscribe(moodboards => {
+        this.spinner.hide();
+        this.moodboards = moodboards;
+        this.accountService.currentUser$.subscribe((loggedUser) => {
+          if (loggedUser) {
+            this.myMoodboards = this.moodboards.filter(m => m.designer.id === loggedUser.id)
+          }
+        })
+      })
+    );
 
     this._route.queryParams.subscribe(params => {
       if(params.section) {
         this.options.initialSlide = params.section;
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
   changeSlide(id) {
@@ -105,6 +115,20 @@ export class MyMoodboardPage implements OnInit {
     });
 
     await popover.present();
+  }
+
+  onFilterChangeMyMoodboard(event){
+    const selectedStyleNames = event.detail.value;
+    this.moodboards$.pipe(take(1)).subscribe(moodboards => {
+      this.accountService.currentUser$.pipe(take(1)).subscribe((loggedUser) => {
+        this.myMoodboards = this.moodboards.filter(m => m.designer.id === loggedUser.id)
+      });
+      if(selectedStyleNames.length > 0) {
+        this.myMoodboards = this.myMoodboards.filter(moodboard => selectedStyleNames.includes(moodboard.style.name));
+      } else {
+        this.myMoodboards = this.myMoodboards;
+      }
+    })
   }
 
   onFilterChange(event){
