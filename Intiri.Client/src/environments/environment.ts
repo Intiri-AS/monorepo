31c28d792ffc9606d986066a9b526bf0033b3384
Raj Karmakar <97rajmath@gmail.com>
2023-06-21 08:52:24 +0530

    update sever url for test

--- Intiri.Client/src/environments/environment.ts
+++ Intiri.Client/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://intiri-test.azurewebsites.net/api/'
-  apiUrl: 'https://localhost:7004/api/'
+  apiUrl: 'http://40.114.78.222/api/'
 };
 
 /*
