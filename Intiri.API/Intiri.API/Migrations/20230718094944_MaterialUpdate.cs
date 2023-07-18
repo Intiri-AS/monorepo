4f32330ce36cf4fcb01c9a1b226af8aace9e4e75
Siddharth Khant <131777310+sidkhant@users.noreply.github.com>
2023-07-18 16:51:49 +0530

    Material Changes

---
+++ Intiri.API/Intiri.API/Migrations/20230718094944_MaterialUpdate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intiri.API.Migrations
+{
+    public partial class MaterialUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Materials",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Provider",
+                table: "Materials",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Materials");
+
+            migrationBuilder.DropColumn(
+                name: "Provider",
+                table: "Materials");
+        }
+    }
+}
