6c4846a672f48c35ed93e7740d10dd0008647c91
Aayushi Shah <aayushi@ymirlabs.com>
2023-08-01 12:20:28 +0000

    environment.ts edited online with Bitbucket

--- Intiri.Client/src/environments/environment.ts
+++ Intiri.Client/src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   // apiUrl: 'https://intiri-test.azurewebsites.net/api/'
   // apiUrl: 'https://localhost:7004/api/'
-  apiUrl: 'http://40.114.78.222/api/'
+  //apiUrl: 'http://40.114.78.222/api/'
+  apiUrl: 'https://qa.app.intiri.no/api/'
 };
 
 /*
