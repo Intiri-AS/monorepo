66426bc9ca5037efed7acad0dd5910ae9e3731c0
Raj Karmakar <raj@ymirlabs.com>
2023-08-09 12:05:32 +0530

    CLIENT: Shuffle styleImages in create project page as per requirement

--- Intiri.Client/src/app/services/CommonUtils.service.ts
+++ Intiri.Client/src/app/services/CommonUtils.service.ts
@@ -12,4 +12,12 @@ export class CommonUtilsService {
     const uniqueValues = Array.from(new Set(arr.filter(value => value !== null && value != 'null')));
     return uniqueValues;
   }
+
+  shuffleArrayElements = <T>(array: T[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 }
