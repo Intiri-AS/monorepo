48c9ede6d200c0b65cb695c00c678b9534c3892a
Siddharth Khant <131777310+sidkhant@users.noreply.github.com>
2023-08-17 21:00:08 +0530

    Create Moodboard PDF Added

---
+++ Intiri.API/Intiri.API/Models/DTO/SlotInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Intiri.API.Models.DTO
+{
+    public class SlotInfo
+    {
+        public string slotId { get; set; } 
+        public string entity { get; set; } 
+        public object entityId { get; set; } 
+        public string entityName { get; set; } 
+        public string entityImagePath { get; set; } 
+    }
+}
